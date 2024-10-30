61c88f7428051ba15e48dcea73695c18afd49a4a
Charalampos Michaelides <xaralamposmicha@gmail.com>
2024-10-30 18:55:59 +0200

    Update_Binance_Api

--- Binance_API.py
+++ Binance_API.py
@@ -2,22 +2,22 @@ import streamlit as st
 import requests
 
 # Binance API URL for 24hr price statistics
-API_URL = "https://api.binance.com/api/v3/ticker/24hr"
+API_URL = "https://api.coincap.io/v2/assets"
 
 
 famous_cryptos = [
-    "BTCUSDT (Bitcoin)",  
-    "ETHUSDT (Ethereum)",  
-    "BNBUSDT (Binance Coin)",  
-    "XRPUSDT (Ripple)",  
-    "ADAUSDT (Cardano)", 
-    "DOGEUSDT (Dogecoin)", 
+    "bitcoin (BTC)",       
+    "ethereum (ETH)",      
+    "binance-coin (BNB)",  
+    "solana (SOL)",        
+    "usd-coin (USDC)",     
+    "dogecoin (DOGE)"      
 ]
 
 
 def get_crypto_data(crypto):
-    crypto = crypto.split()[0] 
-    response = requests.get(f"{API_URL}?symbol={crypto}")
+    crypto = crypto.split()[0].lower()
+    response = requests.get(f"{API_URL}/{crypto}")
     return response.json()
 
 
@@ -29,15 +29,17 @@ def choose_famous_crypto():
    
     if selected_crypto != "":
         crypto_data = get_crypto_data(selected_crypto)
+        
 
         if "error" in crypto_data:
             st.error(crypto_data["error"])
             return
 
-        st.write(f"**Crypto**: {selected_crypto}")
-        st.write(f"**Last Price**: {crypto_data['lastPrice']}")
-        st.write(f"**Price Change (24h)**: {crypto_data['priceChangePercent']}%")
-        st.write(f"**High Price (24h)**: {crypto_data['highPrice']}")
-        st.write(f"**Low Price (24h)**: {crypto_data['lowPrice']}")
-        st.write(f"**Volume**: {crypto_data['volume']}")
-        st.write(f"**Quote Volume**: {crypto_data['quoteVolume']}")
+
+        data = crypto_data['data']
+        st.write(f"**Crypto**: {data['name']} ({data['symbol']})")
+        st.write(f"**Rank**: {float(data['rank'])}")
+        st.write(f"**Price Change (24h)**: {float(data['changePercent24Hr'])}%")
+        st.write(f"**Price Usd**: ${float(data['priceUsd'])}")  
+        st.write(f"**Volume (24h)**: {float(data['volumeUsd24Hr'])}")
+        st.write(f"**Market Cap**: ${float(data['marketCapUsd'])}")
